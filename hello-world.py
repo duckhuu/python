a198a1f1a921ef1d20f33b9e1e6334ee261371fa
duckhuu <40559851+duckhuu@users.noreply.github.com>
2022-12-21 11:22:38 +0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,5 +3,6 @@ from time import sleep
 # the program will print hello world
 #  every 1 second foever
 #while True:
-print("Hello, World")
+#print("Hello, World")
+print("Chào Nguyễn Văn Nguyên Thạnh")
 #    sleep(1)
