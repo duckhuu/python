770aa9da6915182c1ecd5da4c8c03ab994667633
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-06 10:44:53 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,3 +37,13 @@ output "instance_ip_addr_public" {
 	value = aws_instance.web-server.public_ip
 	description = "public ip of server"
 }
+
+output "instance_public_dns" {
+	value = aws_instance.web-server.public_dns
+	description = "public dns of server"
+}
+
+output "instance_private_dns" {
+	value = aws_instance.web-server.private_dns
+	description = "private dns of server"
+}
