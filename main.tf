a1dbe51d96203a624dd29886dc89150467d03626
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-06 10:50:36 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,22 +28,22 @@ resource "aws_instance" "web-server" {
 	 EOF
 }
 
-output "instance_ip_addr_private" {
-	value = aws_instance.web-server.private_ip
-	description = "private ip of server"
-}
+#output "instance_ip_addr_private" {
+#	value = aws_instance.web-server.private_ip
+#	description = "private ip of server"
+#}
 
-output "instance_ip_addr_public" {
-	value = aws_instance.web-server.public_ip
-	description = "public ip of server"
-}
+#output "instance_ip_addr_public" {
+#	value = aws_instance.web-server.public_ip
+#	description = "public ip of server"
+#}
 
-output "instance_public_dns" {
-	value = aws_instance.web-server.public_dns
-	description = "public dns of server"
-}
+#output "instance_public_dns" {
+#	value = aws_instance.web-server.public_dns
+#description = "public dns of server"
+#}
 
-output "instance_private_dns" {
-	value = aws_instance.web-server.private_dns
-	description = "private dns of server"
-}
+#output "instance_private_dns" {
+#	value = aws_instance.web-server.private_dns
+#	description = "private dns of server"
+#}
