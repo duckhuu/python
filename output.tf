61f24a4baae1dc060e40133cc5fb05c9b8ce2b89
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-08 15:06:24 +0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -19,6 +19,6 @@ output "instance_private_dns" {
 }
 
 output "instance_security_group" {
-	value = aws_security_group.allow-http.name
+	value = aws_security_group.allow-http-traffic.name
 	description = "security group"
 }
