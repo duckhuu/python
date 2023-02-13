762645b80e90de20d14ad95f4cce82ded0e05109
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-13 09:53:49 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web-server" {
 	ami	= "ami-08be951cec06726be"
 	instance_type = "t2.micro"
 #	vpc_security_group_ids = "vpc-f7c49692"
-	security_groups = [aws_security_group.allow-http-traffic.name]
+	security_groups = [aws_security_group.allow-http-traffic.id]
 	tags = {
 	 Name = "web-server"
 	}
