87de4fdb730cdddf6006281951a2eacbffe1100b
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-22 12:31:27 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web-server" {
 	ami	= "ami-08be951cec06726be"
 	instance_type = "t2.micro"
 #	vpc_security_group_ids = "vpc-f7c49692"
-	security_groups = [aws_security_group.allow-web-traffic.id]
+#	security_groups = [aws_security_group.allow-web-traffic.id]
 	tags = {
 	 Name = "web-server"
 	}
@@ -49,29 +49,29 @@ resource "aws_instance" "web-server" {
 	 EOF
 }
 
-resource "aws_security_group" "allow-web-traffic" {
+#resource "aws_security_group" "allow-web-traffic" {
 
-	name = "allow_web_traffic"
-	description = "allow web traffic"
+#	name = "allow_web_traffic"
+#	description = "allow web traffic"
 #	vpc_id = "vpc-f7c49692"
-	ingress {
-		description = "Allow port 80"
-		from_port = 80
-		to_port = 80
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-	egress {
-		from_port = 0
-		to_port = 0 
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-	tags = {
-		Name = "allow_web_traffic"	
-	}
+#	ingress {
+#		description = "Allow port 80"
+#		from_port = 80
+#		to_port = 80
+#		protocol = "tcp"
+#		cidr_blocks = ["0.0.0.0/0"]
+#	}
+#	egress {
+#		from_port = 0
+#		to_port = 0 
+#		protocol = "-1"
+#		cidr_blocks = ["0.0.0.0/0"]
+#	}
+#	tags = {
+#		Name = "allow_web_traffic"	
+#	}
 	
-}
+#}
 
 resource "null_resource" "get_public_ip" {
 	provisioner "local-exec" {
@@ -81,15 +81,15 @@ resource "null_resource" "get_public_ip" {
 
 resource "null_resource" "get_public_dns" {
 	provisioner "local-exec" {
-		command = "chmod 777 unit.sh && /bin/sh unit.sh && echo ${aws_instance.web-server.public_dns}  && echo ${aws_security_group.allow-web-traffic.id} && echo ${aws_security_group.allow-web-traffic.name}"
+		command = "chmod 777 unit.sh && /bin/sh unit.sh && echo ${aws_instance.web-server.public_dns}"
 	}
 }
 
-#resource "null_resource" "unit_test" {
-#	provisioner "local-exec" {
-#		command = "curl http://${aws_instance.web-server.public_ip}"
-#	}
-#}
+resource "null_resource" "unit_test" {
+	provisioner "local-exec" {
+		command = "curl http://${aws_instance.web-server.public_ip}:5000"
+	}
+}
 
 #output "instance_ip_addr_private" {
 #	value = aws_instance.web-server.private_ip
