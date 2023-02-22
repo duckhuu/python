5b82683bd9e7296b097d0c1fd1aa42ee068500c2
duckhuu <40559851+duckhuu@users.noreply.github.com>
2023-02-22 12:35:55 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,6 +46,13 @@ resource "aws_instance" "web-server" {
 	 echo $instance_ip_addr_public
 	 echo $instance_ip_addr_private
 	 curl http://$instance_ip_addr_private
+	 sudo apt update
+	 sudo apt install apt-transport-https ca-certificates curl software-properties-common
+	 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+	 sudo apt update
+	 apt-cache policy docker-ce
+	 sudo apt install docker-ce
 	 EOF
 }
 
